b3728f311e85673d23644ea8ac2142f15b65f816
unicoder11 <unicoder11@gmail.com>
2025-07-17 17:53:59 -0300

    template email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import Link from 'next/link'
 import { sendContactEmail } from '@/lib/emailjs'
 
 export default function Home() {
