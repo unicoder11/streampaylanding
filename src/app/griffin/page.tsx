b8cae580fb0314dd2ad0a76c5543414529a00668
unicoder11 <unicoder11@gmail.com>
2025-07-17 17:59:55 -0300

    redirect to contact

--- src/app/griffin/page.tsx
+++ src/app/griffin/page.tsx
@@ -370,7 +370,7 @@ export default function GriffinPage() {
               >
                 <Link href="/#contact">
                   <Button className="bg-black text-white hover:bg-gray-800 rounded-full px-8 py-3 text-lg font-medium">
-                    Contact Griffin Research
+                    Contact for Griffin
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                     </svg>
